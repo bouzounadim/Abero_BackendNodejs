d30042334fe94ae689925170594bcfea43a5de0e
bouzounadim <bouzaffaranadim@gmail.com>
2021-11-19 17:56:36 +0100

    init

---
+++ index.js
@@ -0,0 +1,5 @@
+var  express = require('express');
+const app = express();
+const PORT= process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`))
